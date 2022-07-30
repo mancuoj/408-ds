04f629d6bf4e7974e0a0d9c6530dbf172f4910a5
mancuoj <1337381768@qq.com>
2022-07-30 12:38:03 +0800

    feat: 09

--- 09.cpp
+++ 09.cpp
@@ -3,17 +3,17 @@
 /**
  * 创建一个带有头节点的链表并返回
  */
-LList create_llist(const std::vector<ElemType> &data) {
+List9 create_llist(const std::vector<ElemType> &data) {
     if (data.empty()) {
         return NULL;
     }
 
-    Node *head = (Node *) malloc(sizeof(Node));
+    Node9 *head = (Node9 *) malloc(sizeof(Node9));
     head->link = NULL;
 
-    Node *p = head;
+    Node9 *p = head;
     for (ElemType i: data) {
-        Node *cur = (Node *) malloc(sizeof(Node));
+        Node9 *cur = (Node9 *) malloc(sizeof(Node9));
         cur->data = i;
         cur->link = NULL;
         p->link = cur;
@@ -25,10 +25,11 @@ LList create_llist(const std::vector<ElemType> &data) {
 
 /*
  * bf（Brute Force），即暴力解
+ * 首先扫描一次求出链表长度len，然后重新向后扫描len-k个结点即可
  */
-int search_k_bf(LList list, int k) {
+int search_k_bf(List9 list, int k) {
     int len = 0;
-    Node *p = list->link;
+    Node9 *p = list->link;
     while (p != NULL) {
         p = p->link;
         len++;
@@ -47,6 +48,25 @@ int search_k_bf(LList list, int k) {
 }
 
 /*
- * 最优解
+ * 最优解不一定是真正的最优，只是符合评判标准中的满分
+ * 双指针，一指针先走k步，然后双指针同步，直到前者走到终点，后一指针所在位置即倒数第k个结点
  */
+int search_k(List9 list, int k) {
+    Node9 *p1 = list->link, *p2 = list->link;
+    int cnt = 0;
+    while (p1 != NULL) {
+        if (cnt < k) {
+            cnt++;
+        } else {
+            p2 = p2->link;
+        }
 
+        p1 = p1->link;
+    }
+
+    if (cnt < k) {
+        return 0;
+    }
+    printf("%d\n", p2->data);
+    return 1;
+}
