438116595af08f16df0310029a2e755316b2a799
mancuoj <1337381768@qq.com>
2022-08-06 21:39:28 +0800

    feat: update 14

--- 14.cpp
+++ 14.cpp
@@ -3,24 +3,26 @@
 /*
  * 创建二叉树结点
  */
-BiNode *new_node(int weight) {
-    BiNode *node = (BiNode *) malloc(sizeof(BiNode));
+BiNode *new_node_15(int weight) {
+    auto *node = (BiNode *) malloc(sizeof(BiNode));
     node->weight = weight;
     node->left = NULL;
     node->right = NULL;
     return node;
 }
 
+int wpl;
+
 /**
  * 带权路径长度为每个叶节点的深度与权值之积的总和
  * 所以我们采用前序遍历找到每一个叶节点后计算并返回
  */
 int WPL(BiTree root) {
+    wpl = 0;
     return pre_order_wpl(root, 0);
 }
 
 int pre_order_wpl(BiTree root, int depth) {
-    static int wpl = 0;
     if (root->left == NULL && root->right == NULL) {
         wpl += root->weight * depth;
     }
